935270c17949173ea3e10f7d7ea5d0102857824f
mmoravek <marcel.moravek@yahoo.de>
2024-04-08 20:57:51 +0200

    update inptus

--- src/components/Taginput.vue
+++ src/components/Taginput.vue
@@ -437,17 +437,19 @@ function getFilteredTags(text: string) {
     <h3>Limits</h3>
 
     <o-field label="Limited to 10 characters">
-      <o-taginput maxlength="10" :model-value="['Oruga', 'Vue', 'CSS']" />
+      <o-taginput :maxlength="10" :model-value="['Oruga', 'Vue', 'CSS']" />
     </o-field>
 
     <o-field label="Limited to 5 tags">
-      <o-taginput maxitems="5" :model-value="['One', 'Two', 'Three', 'Four']" />
+      <o-taginput
+        :maxitems="5"
+        :model-value="['One', 'Two', 'Three', 'Four']" />
     </o-field>
 
     <o-field label="Limited to 10 characters and 5 tags">
       <o-taginput
-        maxlength="10"
-        maxitems="5"
+        :maxlength="10"
+        :maxitems="5"
         :model-value="['Red', 'Green', 'Blue', 'White']" />
     </o-field>
   </section>
