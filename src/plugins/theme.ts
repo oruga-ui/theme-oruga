1764a01e82d7db94bb8f6efd2a38d7732bfb73bd
mm <25961416+mlmoravek@users.noreply.github.com>
2024-11-08 09:35:23 +0100

    build: switch from rollup to vite build (#80)

--- src/plugins/theme.ts
+++ src/plugins/theme.ts
@@ -1,1 +1,5 @@
+// import theme style
+import "../assets/scss/oruga-build.scss";
+
 // No theme config to export. Theme includes only css.
+export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,64 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import banner from "vite-plugin-banner";
+import { viteStaticCopy as copy } from "vite-plugin-static-copy";
 
 import { fileURLToPath } from "url";
+import { resolve } from "path";
+
+import pkg from "./package.json" with { type: "json" };
+
+function generate(version: string): string {
+  const preview_build = process.env.ORUGA_PREVIEW_BUILD;
+
+  if (preview_build) {
+    version = `${version} (build ${preview_build})`;
+  }
+
+  return `/*! Oruga Default Theme v${version} | MIT License | github.com/oruga-ui/theme-oruga */`;
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  if (mode === "development") {
+    return {
+      root: __dirname,
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+      },
+    };
+  } else {
+    return {
+      build: {
+        emptyOutDir: true,
+        copyPublicDir: false,
+        lib: {
+          entry: resolve(__dirname, "src/plugins/theme.ts"),
+          name: "OrugaDefaultTheme",
+          fileName: "theme",
+          formats: ["es"],
+        },
+        rollupOptions: {
+          output: {
+            assetFileNames: "oruga.[ext]",
+          },
+        },
+      },
+      css: {
+        // rename default `style.css` to `oruga.css`
+        postcss: { to: "oruga.css" },
+      },
+      plugins: [
+        // copy assets into dist
+        copy({
+          targets: [{ src: "src/assets/scss", dest: "." }],
+        }),
+        // adds a banner to every generated dist file
+        banner(generate(pkg.version)),
+      ],
+    };
+  }
 });
