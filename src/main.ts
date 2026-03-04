5bfcac6bfb498eb71eac346f4241d9e9d75027af
mlmoravek <marcel.moravek@yahoo.de>
2026-03-04 12:32:35 +0100

    feat: update to oruga v0.13.0

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import Oruga from "@oruga-ui/oruga-next";
 import Examples from "@oruga-ui/examples";
 
 // add examples styles
-import "@oruga-ui/examples/dist/examples.css";
+import "@oruga-ui/examples/index.css";
 
 // add theme styles
 import "./assets/scss/theme-build.scss";
@@ -15,6 +15,7 @@ import "./assets/scss/theme-build.scss";
 createApp(App)
     .use(router)
     .use(Oruga, {
+        globalComponents: true,
         iconPack: "fas",
         iconComponent: "vue-fontawesome",
         customIconPacks: {
